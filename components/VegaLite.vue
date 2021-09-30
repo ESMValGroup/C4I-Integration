596f22ee09ab60bd28e1e6918f289b69e11a3ec4
Peter Kalverla <peter.kalverla@gmx.com>
2021-09-30 09:33:43 +0200

    Add event listener (on click, log dataset)

--- components/VegaLite.vue
+++ components/VegaLite.vue
@@ -3,20 +3,25 @@
 </template>
 
 <script>
-
-import vegaEmbed from 'vega-embed'
+import vegaEmbed from "vega-embed";
 
 export default {
-    props: {
-        spec: {
-            type: String,
-            required: true,
-        }
-    },
-    async mounted () {
-        const loadedSpec = await fetch(this.spec).then(res => res.json())
-        vegaEmbed('#vis', loadedSpec).catch(console.warn)
+  props: {
+    spec: {
+      type: String,
+      required: true
     }
-}
-
+  },
+  async mounted() {
+    const loadedSpec = await fetch(this.spec).then(res => res.json());
+    vegaEmbed("#vis", loadedSpec)
+      // https://stackoverflow.com/a/61782407
+      .then(result => {
+        result.view.addEventListener("click", function(event, item) {
+          console.log(item.datum.dataset);
+        });
+      })
+      .catch(console.warn);
+  }
+};
 </script>
